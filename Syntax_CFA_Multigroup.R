f35f220a9c03c4f8534428d5ca0423735640368c
anom90 <111362537+anom90@users.noreply.github.com>
2024-08-12 11:37:01 +0700

    Add files via upload

--- Syntax_CFA_Multigroup.R
+++ Syntax_CFA_Multigroup.R
@@ -1,6 +1,6 @@
 #Jalankan Paket Analisis
-pacman::p_load('semTools', 'lavaan', 'semPlot', 
-               'semptools')
+pacman::p_load('semTools','lavaan','semPlot','semptools', 
+               'lavaanPlot','DiagrammeRsvg','rsvg','png')
 
 #Panggil Data
 data <- read.csv('Data_CFA_2.csv', sep = ',')
@@ -13,150 +13,155 @@ round(mardiaKurtosis(data[,-11]), 3)
 model_cfa_group <- '
                   AK =~ AK_1 + AK_2 + AK_3 + AK_4 + AK_5
                   MK =~ MK_1 + MK_2 + MK_3 + MK_4 + MK_5
-                  KP =~ AK + 1*MK
+                  KP =~ AK + MK
+                  AK ~~ e*AK
+                  MK ~~ e*MK
                   '
 
 #Estimasi Model
-uji_mgcfa <- measurementInvariance(model = model_cfa_group,
-                                  data = data,
-                                  group = "JK",
-                                  strict = T)
+#The measurementInvariance function is deprecated
+#uji_mgcfa <- measurementInvariance(model = model_cfa_group,
+#                                  data = data,
+#                                  group = "JK",
+#                                  strict = T)
+#Ganti sbb:
 
-summary(uji_mgcfa$fit.configural, fit.measure = T, 
-        standardized = T, rsquare = T) #untuk model CI
+#https://bookdown.org/annabrown/psychometricsR/exercise23.html
+#Fitting Baseline / Configural Model
+uji_mgcfa_cfg <- cfa(model = model_cfa_group,data = data,group = 'JK')
+summary(uji_mgcfa_cfg,fit.measure = T, standardized = T, 
+        rsquare = T)
 
-summary(uji_mgcfa$fit.loadings, fit.measure = T, 
-        standardized = T, rsquare = T) #untuk model WI
-
-summary(uji_mgcfa$fit.intercepts, fit.measure = T, 
-        standardized = T, rsquare = T) #untuk model SI
-
-summary(uji_mgcfa$fit.residuals, fit.measure = T, 
-        standardized = T, rsquare = T) #untuk model STI
-
-summary(uji_mgcfa$fit.means, fit.measure = T, 
-        standardized = T, rsquare = T) #untuk model M
+##Fitting Measurement Invariance Model/full measurement invariance model
+uji_mgcfa <- cfa(model_cfa_group, data = data, group = "JK",
+              group.equal = c("loadings", "intercepts", "residuals"))
+summary(uji_mgcfa, fit.measure = T, standardized = T, 
+        rsquare = T)
 
 
-#Reliabilitas
-compRelSEM(uji_mgcfa$fit.configural, higher = "KP")
+#Validitas Konvergen Configural Model
+AVE(uji_mgcfa_cfg)
 
-#Validitas Konvergen
-AVE(uji_mgcfa$fit.configural)
+#Reliabilitas Configural Model
+compRelSEM(uji_mgcfa_cfg, higher = "KP")
 
+#Validitas Konvergen Full Measurement Invariance Model
+AVE(uji_mgcfa)
+
+#Reliabilitas Full Measurement Invariance Model
+compRelSEM(uji_mgcfa, higher = "KP")
+
+#Configural Model
 #1. Visualisasi Model Hipotetik
-par(mfrow = c(1,2))
-semPaths(uji_mgcfa$fit.configural, whatLabels = 'path', 
-        style = 'lisrel', edge.label.cex = 1, 
-        nDigits = 2, edge.color = 'black', 
-        shapeMan = 'rectangle', sizeMan = 8, 
-        sizeMan2 = 5, sizeLat = 9, sizeLat2 = 6, 
-        equalizeManifests = T, nCharNodes = 0, 
-        nCharEdges = 0, rotation = 2, intercepts = F)
-
-pdf('CFA Second Orde-Model Hipotetik%03d.pdf', onefile = F)
-semPaths(uji_mgcfa$fit.configural, whatLabels = 'path', 
-         style = 'lisrel', edge.label.cex = 1, 
-         nDigits = 2, edge.color = 'black', 
-         shapeMan = 'rectangle', sizeMan = 8, 
-         sizeMan2 = 5, sizeLat = 9, sizeLat2 = 6, 
-         equalizeManifests = T, nCharNodes = 0, 
-         nCharEdges = 0, rotation = 2, intercepts = F)
-dev.off()
+p_pa1 <- semPaths(uji_mgcfa_cfg, 'path', style = 'lisrel',
+                 edge.label.cex = .7,nDigits = 3, edge.color = 'black',
+                 layout='tree', sizeMan = 8,sizeMan2 = 3, sizeLat = 8,
+                 rotation = 2,intercepts = F,panelGroups = T,ask = F)
 
 #2. Visualisasi Hitungan Estimasi
-semPaths(uji_mgcfa$fit.configural, whatLabels = 'est', 
-         style = 'lisrel', edge.label.cex = 1, 
-         nDigits = 2, edge.color = 'black', 
-         shapeMan = 'rectangle', sizeMan = 8, 
-         sizeMan2 = 5, sizeLat = 9, sizeLat2 = 6, 
-         equalizeManifests = T, nCharNodes = 0, 
-         nCharEdges = 0, rotation = 2, intercepts = F)
-
-pdf('CFA Second Orde-Hitungan Estimasi%03d.pdf', onefile = F)
-semPaths(uji_mgcfa$fit.configural, whatLabels = 'est', 
-         style = 'lisrel', edge.label.cex = 1, 
-         nDigits = 2, edge.color = 'black', 
-         shapeMan = 'rectangle', sizeMan = 8, 
-         sizeMan2 = 5, sizeLat = 9, sizeLat2 = 6, 
-         equalizeManifests = T, nCharNodes = 0, 
-         nCharEdges = 0, rotation = 2, intercepts = F)
-dev.off()
+p_est1 <- semPaths(uji_mgcfa_cfg, 'path', 'est',style = 'lisrel',
+                  edge.label.cex = .7,nDigits = 3, edge.color = 'black',
+                  layout='tree', sizeMan = 8,sizeMan2 = 3, sizeLat = 8,
+                  rotation = 2,intercepts = F,panelGroups = T,ask = F)
 
 #3. Visualisasi Hitungan Standardized
-semPaths(uji_mgcfa$fit.configural, whatLabels = 'std', 
-         style = 'lisrel', edge.label.cex = 1, 
-         nDigits = 2, edge.color = 'black', 
-         shapeMan = 'rectangle', sizeMan = 8, 
-         sizeMan2 = 5, sizeLat = 9, sizeLat2 = 6, 
-         equalizeManifests = T, nCharNodes = 0, 
-         nCharEdges = 0, rotation = 2, intercepts = F)
+p_std1 <- semPaths(uji_mgcfa_cfg, 'path', 'std',style = 'lisrel',
+                  edge.label.cex = .7,nDigits = 3, edge.color = 'black',
+                  layout='tree', sizeMan = 8,sizeMan2 = 3, sizeLat = 8,
+                  rotation = 2,intercepts = F,panelGroups = T,ask = F)
 
-pdf('CFA Second Orde-Hitungan Standardized%03d.pdf', onefile = F)
-semPaths(uji_mgcfa$fit.configural, whatLabels = 'std', 
-         style = 'lisrel', edge.label.cex = 1, 
-         nDigits = 2, edge.color = 'black', 
-         shapeMan = 'rectangle', sizeMan = 8, 
-         sizeMan2 = 5, sizeLat = 9, sizeLat2 = 6, 
-         equalizeManifests = T, nCharNodes = 0, 
-         nCharEdges = 0, rotation = 2, intercepts = F)
-dev.off()
+#4. Memberi tanda signifikan
+#Multiple-group models are not currently supported
+#sig <- mark_sig(p_std,uji_mgcfa_cfg,alphas = c(`*` = 0.05, `**` = 0.01, `***` = 0.001))
+#plot(sig)
 
-#Simpan
-sink('Hasil Analisis CFA Multiple-Group.txt')
-cat('***Uji Asumsi Normalitas Multivariat***', '\n')
+#5. Visualisasi dengan lavaanPlot
+# stand=T ada masalah
+lp1 <- lavaanPlot(model=uji_mgcfa_cfg,coef=T,stand=F,digits=3,stars=c('regress','latent','covs'),
+                 cov=T,graph_options=list(rankdir='LR'))
+lp1
+
+
+#Full Measurement Invariance Model
+#1. Visualisasi Model Hipotetik
+p_pa2 <- semPaths(uji_mgcfa, 'path', style = 'lisrel',
+                 edge.label.cex = .7,nDigits = 3, edge.color = 'black',
+                 layout='tree', sizeMan = 8,sizeMan2 = 3, sizeLat = 8,
+                 rotation = 2,intercepts = F,panelGroups = T,ask = F)
+
+#2. Visualisasi Hitungan Estimasi
+p_est2 <- semPaths(uji_mgcfa, 'path', 'est',style = 'lisrel',
+                  edge.label.cex = .7,nDigits = 3, edge.color = 'black',
+                  layout='tree', sizeMan = 8,sizeMan2 = 3, sizeLat = 8,
+                  rotation = 2,intercepts = F,panelGroups = T,ask = F)
+
+#3. Visualisasi Hitungan Standardized
+p_std2 <- semPaths(uji_mgcfa, 'path', 'std',style = 'lisrel',
+                  edge.label.cex = .7,nDigits = 3, edge.color = 'black',
+                  layout='tree', sizeMan = 8,sizeMan2 = 3, sizeLat = 8,
+                  rotation = 2,intercepts = F,panelGroups = T,ask = F)
+
+#4. Memberi tanda signifikan
+#Multiple-group models are not currently supported
+#sig <- mark_sig(p_std,uji_mgcfa_cfg,alphas = c(`*` = 0.05, `**` = 0.01, `***` = 0.001))
+#plot(sig)
+
+#5. Visualisasi dengan lavaanPlot
+# stand=T ada masalah
+lp2 <- lavaanPlot(model=uji_mgcfa,coef=T,stand=F,digits=3,stars=c('regress','latent','covs'),
+                 cov=T,graph_options=list(rankdir='LR'))
+lp2
+
+#Simpan output text
+sink('Hasil Syntax_CFA_Multigroup.txt')
+cat('Normalitas Multivariat\n')
 round(mardiaKurtosis(data[,-11]), 3)
+cat('\nConfigural Model\n\n')
+summary(uji_mgcfa_cfg,fit.measure = T, standardized = T, 
+        rsquare = T)
+cat('\nFull Measurement Invariance Model\n\n')
+summary(uji_mgcfa, fit.measure = T, standardized = T, 
+        rsquare = T)
 cat('\n')
-cat('***Ringkasan Hasil CFA Multiple-Group***', '\n')
-summary(uji_mgcfa$fit.configural, fit.measure = T, 
-        standardized = T, rsquare = T)
+cat('AVE Configural Model:\n')
+AVE(uji_mgcfa_cfg)
+cat('\ncompRelSEM Configural Model:\n')
+compRelSEM(uji_mgcfa_cfg, higher = "KP")
 cat('\n')
-cat('***Ringkasan Hasil Perbadingan Model***', '\n')
-uji_mgcfa <- measurementInvariance(model = model_cfa_group,
-                                   data = data,
-                                   group = "JK",
-                                   strict = T)
-cat('\n')
-cat('***Hasil Validitas Konvergen***', '\n')
-AVE(uji_mgcfa$fit.configural)
-cat('\n')
-cat('***Hasil Estimasi Relibilitas***', '\n')
-compRelSEM(uji_mgcfa$fit.configural, higher = "KP")
-cat('\n')
+cat('AVE Full Measurement Invariance Model:\n')
+AVE(uji_mgcfa)
+cat('\ncompRelSEM Full Measurement Invariance Model:\n')
+compRelSEM(uji_mgcfa, higher = "KP")
 sink()
 
-pdf('CFA Second Orde-Model Hipotetik.pdf')
-par(mfrow = c(1,2))
-semPaths(uji_cfa1, whatLabels = 'path', 
-         style = 'lisrel', edge.label.cex = 1, 
-         nDigits = 2, edge.color = 'black', 
-         shapeMan = 'rectangle', sizeMan = 8, 
-         sizeMan2 = 5, sizeLat = 9, sizeLat2 = 6, 
-         equalizeManifests = T, nCharNodes = 0, 
-         nCharEdges = 0, rotation = 2, intercepts = F)
+##Simpan output gambar
+pdf('Hasil Syntax_CFA_Multigroup.pdf',paper = 'a4')
+p_pa1 <- semPaths(uji_mgcfa_cfg, 'path', style = 'lisrel',
+                  edge.label.cex = .7,nDigits = 3, edge.color = 'black',
+                  layout='tree', sizeMan = 8,sizeMan2 = 3, sizeLat = 8,
+                  rotation = 2,intercepts = F,panelGroups = T,ask = F)
+p_est1 <- semPaths(uji_mgcfa_cfg, 'path', 'est',style = 'lisrel',
+                   edge.label.cex = .7,nDigits = 3, edge.color = 'black',
+                   layout='tree', sizeMan = 8,sizeMan2 = 3, sizeLat = 8,
+                   rotation = 2,intercepts = F,panelGroups = T,ask = F)
+p_std1 <- semPaths(uji_mgcfa_cfg, 'path', 'std',style = 'lisrel',
+                   edge.label.cex = .7,nDigits = 3, edge.color = 'black',
+                   layout='tree', sizeMan = 8,sizeMan2 = 3, sizeLat = 8,
+                   rotation = 2,intercepts = F,panelGroups = T,ask = F)
+p_pa2 <- semPaths(uji_mgcfa, 'path', style = 'lisrel',
+                  edge.label.cex = .7,nDigits = 3, edge.color = 'black',
+                  layout='tree', sizeMan = 8,sizeMan2 = 3, sizeLat = 8,
+                  rotation = 2,intercepts = F,panelGroups = T,ask = F)
+p_est2 <- semPaths(uji_mgcfa, 'path', 'est',style = 'lisrel',
+                   edge.label.cex = .7,nDigits = 3, edge.color = 'black',
+                   layout='tree', sizeMan = 8,sizeMan2 = 3, sizeLat = 8,
+                   rotation = 2,intercepts = F,panelGroups = T,ask = F)
+p_std2 <- semPaths(uji_mgcfa, 'path', 'std',style = 'lisrel',
+                   edge.label.cex = .7,nDigits = 3, edge.color = 'black',
+                   layout='tree', sizeMan = 8,sizeMan2 = 3, sizeLat = 8,
+                   rotation = 2,intercepts = F,panelGroups = T,ask = F)
 dev.off()
 
-pdf('CFA Second Orde-Hitungan Estimasi.pdf')
-par(mfrow = c(1,2))
-semPaths(uji_cfa1, whatLabels = 'est', 
-         style = 'lisrel', edge.label.cex = 1, 
-         nDigits = 2, edge.color = 'black', 
-         shapeMan = 'rectangle', sizeMan = 8, 
-         sizeMan2 = 5, sizeLat = 9, sizeLat2 = 6, 
-         equalizeManifests = T, nCharNodes = 0, 
-         nCharEdges = 0, rotation = 2, intercepts = F)
-dev.off()
-
-pdf('CFA Second Orde-Hitungan Standardize.pdf')
-par(mfrow = c(1,2))
-semPaths(uji_cfa1, whatLabels = 'std', 
-         style = 'lisrel', edge.label.cex = 1, 
-         nDigits = 2, edge.color = 'black', 
-         shapeMan = 'rectangle', sizeMan = 8, 
-         sizeMan2 = 5, sizeLat = 9, sizeLat2 = 6, 
-         equalizeManifests = T, nCharNodes = 0, 
-         nCharEdges = 0, rotation = 2, intercepts = F)
-dev.off()
-
-
-
+#Simpan output gambar lavaanPlot
+save_png(lp1,'Hasil Syntax_CFA_Multigroup1.png')
+save_png(lp2,'Hasil Syntax_CFA_Multigroup2.png')
